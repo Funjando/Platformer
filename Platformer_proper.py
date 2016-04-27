6c9ed8732b9cebe932d9829cee42f2dd29ddea08
Funjando <andojuuichi@gmail.com>
2016-04-27 13:49:27 -0400

    Updated from Brython Server: 4/27/2016 1:50:47 PM

--- Platformer_proper.py
+++ Platformer_proper.py
@@ -1,1 +1,25 @@
+"""
+platformer.py
+Author: <your name here>
+Credit: <list sources used, if any>
+Assignment:
+Write and submit a program that implements the sandbox platformer game:
+https://github.com/HHS-IntroProgramming/Platformer
+"""
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+
+
+
+
+
+
+
+
+
+
+
 
